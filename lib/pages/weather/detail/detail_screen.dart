e565fd8c2281b7182c0103a09d9560d69d4c0577
christopher-glitch <christph.glitch@gmail.com>
2024-02-27 04:51:26 +0900

    Bug fix

--- lib/pages/weather/detail/detail_screen.dart
+++ lib/pages/weather/detail/detail_screen.dart
@@ -1,28 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kasairu/controller/provider/repository/favorite_provider.dart';
+import 'package:kasairu/controller/provider/repository/settings_provider.dart';
+import 'package:kasairu/controller/service/judge/judge_umbrella.dart';
+import 'package:kasairu/controller/service/judge/result_judge.dart';
 import 'package:kasairu/models/place/place.dart';
 import 'package:kasairu/pages/weather/detail/weather_ui.dart';
 import 'package:kasairu/pages/weather/detail/detail_header.dart';
 
 import '../../../models/onecall/onecall_hourly.dart';
-import '../../../controller/service/judge/result_judge.dart';
 
 class DetailScreen extends ConsumerWidget {
   final List<OneCallHourly> entry;
   final Place place;
-  final ResultJudge result;
   
   const DetailScreen({
       required this.entry,
       required this.place,
-      required this.result,
       super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     List<Place> favoriteList = ref.watch(favoriteProvider);
-    
+    List<int> settingRainJudge = ref.watch(settingsProvider);
+
+    ResultJudge result = judgeTakeUmbrella(entry, settingRainJudge);
     return Scaffold(
         appBar: const DetailScreenHeader(),
         backgroundColor: Colors.white,
